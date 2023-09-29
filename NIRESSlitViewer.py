fe2b4047b469ee300e9e024daf31d13e203df5b8
jdpelletier <john.pelletier@ymail.com>
2023-09-28 22:20:43 -1000

    Update NIRESSlitViewer.py

--- NIRESSlitViewer.py
+++ NIRESSlitViewer.py
@@ -385,7 +385,7 @@ class FitsViewer(QtGui.QMainWindow):
     def fileIsCurrentlyLocked(self):
         print(f'{self.go} locked')
         locked = True
-        if self.go == 0:
+        if self.go.read() == 0:
             print(f'{self.go} unlocked')
             locked = False
         return locked

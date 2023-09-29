9abaec7450697309a719d3824484f564ea982f30
jdpelletier <john.pelletier@ymail.com>
2023-09-29 13:31:29 -1000

    Update NIRESSlitViewer.py

--- NIRESSlitViewer.py
+++ NIRESSlitViewer.py
@@ -361,9 +361,7 @@ class FitsViewer(QtGui.QMainWindow):
         previous = fits.getdata('/s/sdata1500/nires3/2023sep29//v230929_0035.fits')
         # previous = fits.getdata(str(self.previous_image))
         subtracted = data - previous
-        new_img = self.img.load_data(subtracted)
-        print(new_img)
-        self.fitsimage.set_image(new_img)
+        self.fitsimage.set_image(load_data(subtracted))
         # self.load_file(self.writeFits(header, subtracted))
         # load_data(subtracted)
 

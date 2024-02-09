ceb7098d6eb2456e7b85004235754a88220e366e
jdpelletier <john.pelletier@ymail.com>
2024-02-08 22:41:14 -1000

    Update NIRESSlitViewer.py

--- NIRESSlitViewer.py
+++ NIRESSlitViewer.py
@@ -348,9 +348,9 @@ class Cuts(Widgets.Box):
         self.tags = [self._new_cut]
         self.cutstag = self._new_cut
         # self.w.cuts.append_text(self._new_cut)
-        self.select_cut(self._new_cut)
+        # self.select_cut(self._new_cut)
         self.save_cuts.set_enabled(False)
-        self.cuts_plot.clear()
+        # self.cuts_plot.clear()
         # plot cleared in replot_all() if no more cuts
         self.replot_all()
 

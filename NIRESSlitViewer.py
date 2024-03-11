09048dfd3fe2ae276381b7f608de49e01f3fd130
jdpelletier <john.pelletier@ymail.com>
2024-03-11 01:31:45 -1000

    Update NIRESSlitViewer.py

--- NIRESSlitViewer.py
+++ NIRESSlitViewer.py
@@ -1127,7 +1127,6 @@ class FitsViewer(QtGui.QMainWindow):
             ra_txt, dec_txt = image.pixtoradec(fits_x, fits_y,
                                                format='str', coords='fits')
         except Exception as e:
-            print(e)
             self.logger.warning("Bad coordinate conversion: %s" % (
                 str(e)))
             ra_txt = 'BAD WCS'

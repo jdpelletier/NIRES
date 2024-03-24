3be5a30fcbdc36541a9e4e82c559449b1c51d3c0
jdpelletier <john.pelletier@ymail.com>
2024-03-23 23:04:26 -1000

    Update NIRESSpecViewer.py

--- NIRESSpecViewer.py
+++ NIRESSpecViewer.py
@@ -1128,6 +1128,7 @@ class FitsViewer(QtGui.QMainWindow):
         vw = QtGui.QWidget()
         self.setCentralWidget(vw)
         vw.setLayout(vbox)
+        vw.resize(1200,1200)
 
         fi.set_callback('cursor-changed', self.motion_cb)
         fi.add_callback('cursor-down', self.btndown)

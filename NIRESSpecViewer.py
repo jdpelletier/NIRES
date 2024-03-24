d319a9246f12f83b8d34cea69eae721a889b1a59
jdpelletier <john.pelletier@ymail.com>
2024-03-23 19:13:44 -1000

    Update NIRESSpecViewer.py

--- NIRESSpecViewer.py
+++ NIRESSpecViewer.py
@@ -1175,11 +1175,14 @@ class FitsViewer(QtGui.QMainWindow):
         
     def drag_cb(self, viewer, button, data_x, data_y):
         low, high = viewer.get_cut_levels()
+        if data
         dx = data_x - self.xclick
         dy = data_y - self.yclick
         low = low + dx
         high = high + dy
         viewer.cut_levels(low, high)
+        self.xclick = dx
+        self.yclick = dy
 
 
     def quit(self, *args):

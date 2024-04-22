eacaee05508ff81a8fe8fb963c03fdd857b9332d
jdpelletier <john.pelletier@ymail.com>
2024-04-21 22:31:51 -1000

    Update NIRESSpecViewer.py

--- NIRESSpecViewer.py
+++ NIRESSpecViewer.py
@@ -1451,7 +1451,7 @@ class FitsViewer(QtGui.QMainWindow):
         if len(fileName) != 0:
             self.load_file(fileName)
             image = self.fitsimage.get_image()
-            print(image.name)
+            print(image.get_name())
 
     def cuts_popup(self):
         if self.c != None:

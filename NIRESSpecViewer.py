4766b76e46c025637370c3b0135989c76188217d
jdpelletier <john.pelletier@ymail.com>
2024-04-21 21:10:51 -1000

    Update NIRESSpecViewer.py

--- NIRESSpecViewer.py
+++ NIRESSpecViewer.py
@@ -1096,7 +1096,7 @@ class FitsViewer(QtGui.QMainWindow):
         colormenu = menubar.addMenu("Colors")
         for name in cmap.get_names():
             item = QtGui.QAction(name, menubar)
-            item.triggered.connect(self.cmap_change)
+            item.triggered.connect(self.cmap_change(name))
             colormenu.addAction(item)
 
 

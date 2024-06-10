bd84c6cfbc6de1230f1475bb349f47462869f37e
jdpelletier <john.pelletier@ymail.com>
2024-06-09 21:32:00 -1000

    Update NIRESSlitViewer.py

--- NIRESSlitViewer.py
+++ NIRESSlitViewer.py
@@ -911,7 +911,7 @@ class MathWindow(Widgets.Box):
         # x = screen.width()/2
         # y = screen.height()/2
         # self.move(x, y)
-        self.resize(500, 0)
+        self.resize(500, 500)
 
         self.sdiff_done = False
 

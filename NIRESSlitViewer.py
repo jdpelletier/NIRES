5f95398bc1204f80e43ba88585ef26a33800d691
jdpelletier <john.pelletier@ymail.com>
2024-06-09 21:33:45 -1000

    Update NIRESSlitViewer.py

--- NIRESSlitViewer.py
+++ NIRESSlitViewer.py
@@ -1739,7 +1739,7 @@ def main():
     logger = log.get_logger("NIRESSlitViewer", log_stderr=True, level=40)
 
     w = FitsViewer(logger)
-    w.resize(1000,800)
+    w.resize(700,800)
     w.show()
     app.setActiveWindow(w)
     w.raise_()

71af04ec0b1fec814be1c1476d9e3eb43a093f1a
jdpelletier <john.pelletier@ymail.com>
2024-08-17 20:41:38 -1000

    Update NIRESSpecViewer.py

--- NIRESSpecViewer.py
+++ NIRESSpecViewer.py
@@ -1194,8 +1194,8 @@ class FitsViewer(QtGui.QMainWindow):
         # readout_hbox.addWidget(self.wcolor)
 
         self.bd = fi.get_bindings()
-        self.bd.enable_all(True)
-        self.bd.enable_rotate(False)
+        self.bd.enable_all(False)
+        # self.bd.enable_rotate(False)
         vbox = QtGui.QVBoxLayout()
         vbox.setContentsMargins(0, 0, 0, 0)
         vbox.setObjectName("vbox")

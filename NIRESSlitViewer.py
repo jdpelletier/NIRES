beb53e6099404445238a07904a26cfb638745e5c
jdpelletier <john.pelletier@ymail.com>
2024-08-19 01:23:14 -1000

    Update NIRESSlitViewer.py

--- NIRESSlitViewer.py
+++ NIRESSlitViewer.py
@@ -1765,7 +1765,7 @@ class FitsViewer(QtGui.QMainWindow):
         # x = wd//2
         # ra = float(header['RA'])
         # dec = float(header['DEC'])
-        self.rot = float(header['ROTPOSN']+180)
+        self.rot = float(header['ROTPOSN']-180)
         print(self.rot)
         # w.wcs.crpix = [y, x]
         # w.wcs.cdelt = np.array([-0.05, 0.05])

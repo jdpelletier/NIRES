8619d960f997d405f2901ad5f80f1db7ce0f4314
jdpelletier <john.pelletier@ymail.com>
2024-08-27 11:08:39 -1000

    Update NIRESSpecViewer.py

--- NIRESSpecViewer.py
+++ NIRESSpecViewer.py
@@ -1384,7 +1384,7 @@ class FitsViewer(QtGui.QMainWindow):
             print(f"y = {len(fy)}")
             wavelength = str(wavelength)
             value = str(value)
-            text = f"X: {fx : <7} Y: {fy : <7}  Value: {value : <10}  Wavelength: {wavelength}"
+            text = f"X: {fx :  <7} Y: {fy :  <7}  Value: {value :  <10}  Wavelength: {wavelength}"
             self.readout.setText(text)
         
     def drag_cb(self, viewer, button, data_x, data_y):

d84271a512c5c4a194700c9f0d4a51250502f0f6
jdpelletier <john.pelletier@ymail.com>
2024-09-01 03:07:45 -1000

    Update NIRESSpecViewer.py

--- NIRESSpecViewer.py
+++ NIRESSpecViewer.py
@@ -954,7 +954,7 @@ class FitsViewer(QtGui.QMainWindow):
             self.file_info.setText(f"File: {filepath}")
             self.base_zoom = self.fitsimage.get_zoom()
         except io_fits.FITSError:
-            self.file_info.setText(f"File: error loading, wait for next image")
+            self.file_info.setText(f"File: error loading, empty image")
         except OSError:
             self.file_info.setText(f"File: error loading, possible server crash")
 

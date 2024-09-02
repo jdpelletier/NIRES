0e870f2913bb13d69cdf2e93eb5ba1bb066abbd2
jdpelletier <john.pelletier@ymail.com>
2024-09-02 01:48:36 -1000

    Update NIRESSpecViewer_v2.py

--- NIRESSpecViewer_v2.py
+++ NIRESSpecViewer_v2.py
@@ -1032,6 +1032,7 @@ class FitsViewer(QtGui.QMainWindow):
     
     def scan(self, file_callback):
         in_file = str(self.dispname)
+        file_callback.emit(in_file)
         while self.scanning:
             cur_file = str(self.dispname)
             if in_file != cur_file:
